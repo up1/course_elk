369344c99effbda2f7e75d955806619a7dd41616
up1 <somkiat.p@gmail.com>
2019-06-20 08:46:09 +0700

    Demo code for logging with spring-boot project

---
+++ workshop/java-spring/demo/Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:8u111-jdk-alpine
+WORKDIR /app
+COPY ./target/demo.jar /app/demo.jar
+ENTRYPOINT ["java","-jar","/app/demo.jar"]
--- workshop/java-spring/demo/src/main/java/com/example/demo/HelloController.java
+++ workshop/java-spring/demo/src/main/java/com/example/demo/HelloController.java
@@ -1,4 +1,32 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hello")
+    public String hello() {
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
+        return "Hello RESTFul API from Spring boot";
+    }
+
+    @GetMapping("/hello-error")
+    public String error() {
+        try {
+            throw new Exception("Error");
+        } catch (Exception e) {
+            logger.error("error message with stack trace ", e);
+        }
+        return "Error";
+    }
+
 }
